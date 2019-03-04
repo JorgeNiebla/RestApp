351de36d66567b4cdb9fa5263374653e19592347
jorge niebla <jorge.niebla@ge.com>
2019-03-04 15:12:19 -0600

    Version Inicial

---
+++ RestApp/Controllers/TodoListController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoListController : ControllerBase
+    {
+        private readonly TodoListService _todoListService;
+
+        public TodoListController(TodoListService todoListService)
+        {
+            _todoListService = todoListService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_todoListService.GetTodoList);
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] string item)
+        {
+            _todoListService.Add(item);
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete([FromBody]int index)
+        {
+            _todoListService.Remove(index);
+
+            return Ok();
+        }
+    }
+}
---
+++ RestApp/TodoListService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApp
+{
+    public class TodoListService
+    {
+        private IList<string> _todoList;
+
+        public TodoListService()
+        {
+            _todoList = new List<string>();
+        }
+
+        public void Add(string item)
+        {
+            _todoList.Add(item);
+        }
+
+        public void Remove(int index)
+        {
+            _todoList.RemoveAt(index);
+        }
+
+        public IList<string> GetTodoList {
+            get
+            {
+                return _todoList;
+            }
+        }
+    }
+}
