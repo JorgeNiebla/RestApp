94f0042a160d9a9fd782e8ea33e4cef071530645
jorge niebla <jorge.niebla@nbgz.me>
2019-03-04 23:00:41 -0600

    cambios

--- RestApp/wwwroot/js/site.js
+++ RestApp/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$().ready(function () {
+    $('#btnAdd').click(function () {
+        $.ajax({
+            url: 'api/TodoList',
+            data: "'" + $('#txtAdd').val() + "'",
+            contentType: "application/json",
+            method: 'POST'
+        }).done(function () {
+            $('#todoList').append('<li>' + $('#txtAdd').val() + '<button type="button" class="btn btn-danger btn-xs btnRemove"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>Eliminar</button></li>');
+        });
+    });
+
+    $('button.btnRemove').on('click', function () {
+        $(this).closest('li').remove();
+    });
+});
+
