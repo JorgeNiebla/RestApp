3aa6fe1baeadff7d72459b90ee1d1622dc26a585
jorge niebla <jorge.niebla@ge.com>
2019-03-05 08:11:24 -0600

    Se agrego Compilacion

---
+++ Compilacion/wwwroot/js/site.js
@@ -0,0 +1,43 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$().ready(function () {
+    function remove() {
+        var currentLi = $(this).closest('li');
+
+        $.ajax({
+            url: 'api/TodoList',
+            data: "'" + $('#todoList li').index(currentLi) + "'",
+            contentType: "application/json",
+            method: 'DELETE'
+        }).done(function () {
+            currentLi.remove();
+        });
+    }
+
+    function add() {
+        $.ajax({
+            url: 'api/TodoList',
+            data: "'" + $('#txtAdd').val() + "'",
+            contentType: "application/json",
+            method: 'POST'
+        }).done(function () {
+            $('#todoList').append('<li>' + $('#txtAdd').val() + ' <button type="button" class="btn btn-danger btn-xs btnRemove"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button></li>');
+            $('#txtAdd').val('');
+
+            $('.btnRemove').off('click', remove);
+            $('.btnRemove').on('click', remove);
+        });
+    }
+
+    
+    $('#btnAdd').click(add);
+
+    $('#txtAdd').keypress(function (event) {
+        if (event.which === 13) {
+            add();
+        }
+    });
+});
+
